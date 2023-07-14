b7012e5d48378b34bb691d03a8c4c5c3d627f09e
Michael Schlueter <schluet0r4life@gmail.com>
2023-07-14 16:29:07 +0200

    add profile page jsx/styles

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -100,6 +100,14 @@ const TweetCard = ({
         };
       };
       trpcUtils.tweet.infiniteFeed.setInfiniteData({}, updateData);
+      trpcUtils.tweet.infiniteFeed.setInfiniteData(
+        { onlyFollowing: true },
+        updateData
+      );
+      trpcUtils.tweet.infiniteProfileFeed.setInfiniteData(
+        { userId: user.id },
+        updateData
+      );
     },
   });
 
